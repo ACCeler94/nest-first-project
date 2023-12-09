b8146515d7dcebfdefa81b4b474cd97381119452
Artur C <araghast94@icloud.com>
2023-12-09 16:04:24 +0100

    Add validation to create product method

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from 'src/db';
 import { CreateProductDTO } from './dtos/create-product.dto';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -12,11 +13,11 @@ export class ProductsController {
     return this.productsService.getAll();
   }
   @Get('/:id')
-  getById(@Param('id') id: 'string'): any {
+  getById(@Param('id', new ParseUUIDPipe()) id: 'string'): any {
     return this.productsService.getById(id);
   }
   @Delete('/:id')
-  deleteById(@Param('id') id: 'string'): any {
+  deleteById(@Param('id', new ParseUUIDPipe()) id: 'string'): any {
     this.productsService.deleteById(id);
     return { success: true };
   }
